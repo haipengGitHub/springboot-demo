288fbea9db33ac0af98074266b9e35d70b345bed
Lee <Lee@DESKTOP-F2I4G08>
2018-12-11 14:43:13 +0800

    first commit

---
+++ src/main/java/com/demo/HellowController.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Lee
+ * 2018年12月7日
+    * 测试Spring Boot控制器
+ */
+@RestController
+@RequestMapping("test")
+public class HellowController {
+	
+	@RequestMapping("hellow")
+	public String hellow() {
+		return "Hellow Spring Boot!";
+	}
+}
